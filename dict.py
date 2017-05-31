773219d527726c2390c29db2c5b125da4d5bf6b5
FrancisTan <499166435@qq.com>
2017-05-31 18:10:04 +0800

    make basic directory for basic files

--- dict.py
+++
@@ -1,6 +0,0 @@
-d = {'Michael': 95, 'Bob': 75, 'Tracy': 85}
-print(d['Michael'])
-t = 'Toms' in d
-print(t)
-b = d.get('Bob')
-print(b)
--- function.py
+++
@@ -1,7 +0,0 @@
-# define a function
-PI = 3.14159265
-def squre(r):
-    return PI * r * r;
-
-s = squre(3)
-print(s)
